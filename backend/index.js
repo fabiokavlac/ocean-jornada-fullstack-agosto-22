7e16ff9a5c63208b05a010546ec3003dd66fb178
Fabio Kavlac <75172308+fabiokavlac@users.noreply.github.com>
2022-08-18 12:15:05 -0300

    Input DB

--- backend/index.js
+++ backend/index.js
@@ -42,23 +42,23 @@ async function main() {
   // Nosso backend armazena as pontuações das jogadas
   // Criar a lista com as pontuações
 
-  // const lista = [
-  //   {
-  //     id: 1,
-  //     nome: "Paulo",
-  //     pontos: 21,
-  //   },
-  //   {
-  //     id: 2,
-  //     nome: "Daniel",
-  //     pontos: 52,
-  //   },
-  //   {
-  //     id: 3,
-  //     nome: "Beatriz",
-  //     pontos: 97,
-  //   },
-  // ];
+   const lista = [
+     {
+       id: 1,
+       nome: "Paulo",
+       pontos: 21,
+     },
+     {
+       id: 2,
+       nome: "Daniel",
+       pontos: 52,
+     },
+     {
+       id: 3,
+       nome: "Beatriz",
+       pontos: 97,
+     },
+   ];
 
   // Endpoint READ ALL - [GET] /pontuacoes
   app.get("/pontuacoes", async function (req, res) {
